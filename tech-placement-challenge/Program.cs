3e680cae9c13c4fda99f7f2739c07c30ffb29533
Humza Khan <humza.khan890@gmail.com>
2019-11-19 23:40:48 +0000

    Removed Item class

--- tech-placement-challenge/Program.cs
+++ tech-placement-challenge/Program.cs
@@ -7,58 +7,53 @@ namespace tech_placement_challenge
     {
         static void Main(string[] args)
         {
-            List<Item> pricingRule = new List<Item>();
+            List<PricingRule> pricingRules = new List<PricingRule>();
 
-            pricingRule.Add(new PricingRule("A", 8, "None"));
-            pricingRule.Add(new PricingRule("B", 12, "2 for £20.00"));
-            pricingRule.Add(new PricingRule("C", 4, "3 for £10.00"));
-            pricingRule.Add(new PricingRule("D", 7, "Buy 1 get 1 free"));
-            pricingRule.Add(new PricingRule("E", 5, "3 for the price of 2"));
+            pricingRules.Add(new PricingRule("A", 8, "None"));
+            pricingRules.Add(new PricingRule("B", 12, "2 for £20.00"));
+            pricingRules.Add(new PricingRule("C", 4, "3 for £10.00"));
+            pricingRules.Add(new PricingRule("D", 7, "Buy 1 get 1 free"));
+            pricingRules.Add(new PricingRule("E", 5, "3 for the price of 2"));
 
-            //UnidaysDiscountChallenge example = new UnidaysDiscountChallenge(pricingRule);
+            UnidaysDiscountChallenge example = new UnidaysDiscountChallenge(pricingRules);
         }
     }
 
     class UnidaysDiscountChallenge
     {
-        public List<double> basket;
+        public List<PricingRule> pricingRules;
+        public List<string> basket;
 
-        public UnidaysDiscountChallenge(List<Item> pricingRule)
+        public UnidaysDiscountChallenge(List<PricingRule> PricingRules)
         {
-
+            this.pricingRules = PricingRules;
         }
 
-        static void AddToBasket()
+        static void AddToBasket(string item)
         {
-
+            
         }
 
         static void CalculateTotalPrice()
         {
 
         }
+
+        
     }
 
-    class Item
+    class PricingRule
     {
-        public string name;
+        public string item;
         public double price;
-
-        public Item(string Name, double Price)
-        {
-            this.name = Name;
-            this.price = Price;
-        }
-
-    }
-
-    class PricingRule : Item
-    {
         public string discount;
 
-        public PricingRule(string Name, double Price, string Discount) : base(Name, Price)
+        public PricingRule(string Item, double Price, string Discount)
         {
+            this.item = Item;
+            this.price = Price;
             this.discount = Discount;
         }
+
     }
 }
