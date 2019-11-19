a104008b8d9cdcf84d42d0bdbaa4e8389a5fa5ef
Humza Khan <humza.khan890@gmail.com>
2019-11-19 22:15:31 +0000

    Added project
    Create new project

---
+++ tech-placement-challenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tech_placement_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class UnidaysDiscountChallenge
+    {
+        static void UniversityDiscountChallenge()
+        {
+
+        }
+
+        static void AddToBasket()
+        {
+
+        }
+
+        static void CalculateTotalPrice()
+        {
+
+        }
+    }
+}
